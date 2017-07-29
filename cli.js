bdc6c50372f50a2a2bf54f4edb52e8a53b3fb532
eight <eight04@gmail.com>
2017-07-29 17:51:24 +0800

    Add: warning message to --update option

--- cli.js
+++ cli.js
@@ -2,20 +2,22 @@
 
 var neodoc = require("neodoc");
 
-var args = neodoc.run(`
+var args = neodoc.run(`Build userscript's meta from package.json file.
+
 usage:
   userscript-meta [--no-package] [--read=<file>...] [--update=<file>]
                   [--output=<file>] [--json]
-  
+
 options:
   -n --no-package Don't extract data from package.json
-  
+
   -r --read       Read metadata from files. Support json or any text file
                   containing userscript metadata block.
-				  
+
   -u --update     Update the metadata block in the file, instead of writing to
-                  output.
-				  
+                  output. WARNING: If you want to preserved the original
+                  metas, please -read the file first.
+
   -o --output     Write output meta block to file. If not provided, writing to
                   stdout.
 
